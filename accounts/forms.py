8582b2783720dd5e3453e4ab13eee1dd1d2814cb
yootina <tinayoo1004@gmail.com>
2023-11-09 14:06:50 +0900

    accounts login 기능 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ('username', 'profile_image', )
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
-
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
 
 def signup(request):
@@ -13,6 +13,21 @@ def signup(request):
     else:
         form = CustomUserCreationForm()
     
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts_form.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('posts:index')
+
+    else:
+        form = CustomAuthenticationForm()
     context = {
         'form': form,
     }
