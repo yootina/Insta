50b23f608cbad9a5457b688dc90a767a51e708d6
yootina <tinayoo1004@gmail.com>
2023-11-09 13:46:21 +0900

    accounts Signup 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:index')
+
+    else:
+        form = CustomUserCreationForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts_form.html', context)
