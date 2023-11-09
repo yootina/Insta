77275a026a88e14ae82762c48309ba5371ce5e98
yootina <tinayoo1004@gmail.com>
2023-11-09 10:02:33 +0900

    Post모델링

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='image/%Y/%m')
